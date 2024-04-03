4cd0d4dd8c1b9aebb50139a81a7c6a3b0d5d8bee
kianwasabi <55065075+kianwasabi@users.noreply.github.com>
2024-04-03 21:31:30 +0200

    Add venv setup and mining class

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-# create .venv
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
 
-# install dependencies
 
 
--- webservice/httpservice.py
+++ webservice/httpservice.py
@@ -10,3 +10,26 @@ class PowerGrid:
         self.kilowatt = payload['args']['kilowatt']
         return self.kilowatt
 
+class Mining:
+    def __init__(self):
+        self.price = 0
+        self.hashrate = 0
+        self.memepoolblocks = 0
+    def get_mempoolblocks(self):
+        response = requests.get("https://mempool.space/api/v1/fees/mempool-blocks")
+        payload = response.json()
+        print(payload)
+        self.memepoolblocks = payload['args']['0']['totalFees']
+        return self.memepoolblocks
+    def get_price(self):
+        response = requests.get("https://mempool.space/api/v1/prices")
+        payload = response.json()
+        print(payload)
+        self.price = payload['args']['EUR']
+        return self.price
+    def get_hashrate(self):
+        response = requests.get("https://mempool.space/api/v1/mining/hashrate/1d")
+        payload = response.json()
+        print(payload)
+        self.hashrate = payload['args']['currentHashrate']
+        return self.price
